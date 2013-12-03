7cc3635561819be77693eb8aa7e6719eb349e705
Dennis Schridde <devurandom@gmx.net>
2013-12-03 15:46:10 +0100

    Add struct siginfo to C.lua, since it is needed on Ubuntu

--- C.lua
+++ C.lua
@@ -5,6 +5,42 @@ union sigval {
   int sival_int;
   void *sival_ptr;
 };
+struct siginfo {
+  int si_signo;
+  int si_errno;
+  int si_code;
+  union {
+    int _pad[29];
+    struct {
+      int si_pid;
+      unsigned int si_uid;
+    } _kill;
+    struct {
+      int si_tid;
+      int si_overrun;
+      union sigval si_sigval;
+    } _timer;
+    struct {
+      int si_pid;
+      unsigned int si_uid;
+      union sigval si_sigval;
+    } _rt;
+    struct {
+      int si_pid;
+      unsigned int si_uid;
+      int si_status;
+      long int si_utime;
+      long int si_stime;
+    } _sigchld;
+    struct {
+      void *si_addr;
+    } _sigfault;
+    struct {
+      long int si_band;
+      int si_fd;
+    } _sigpoll;
+  } _sifields;
+};
 ]]
 
 return ffi.C
